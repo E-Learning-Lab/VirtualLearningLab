65f18c62fdc342df2a266c3621d5e6cc86ce822c
iamTanTan <70108451+iamTanTan@users.noreply.github.com>
2021-10-12 19:55:53 -0700

    heroku changes

--- virtuallearninglab/settings.py
+++ virtuallearninglab/settings.py
@@ -32,7 +32,7 @@ with open('secret_key.txt') as f:
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['207.154.236.200', 'wwww.elearning-lab.org', 'elearning-lab.org','virtual-learning-lab.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['sudo nginx -t && sudo systemctl restart nginx','radiant-meadow-94865.herokuapp.com', '207.154.236.200', 'wwww.elearning-lab.org', 'elearning-lab.org','virtual-learning-lab.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
